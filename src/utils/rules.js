965f41f3c31735447c0649336731337433f16662
Vincent Lejtzén <vincent@lejtzendesign.se>
2020-02-21 13:50:39 +0100

    Change rules

--- src/utils/rules.js
+++ src/utils/rules.js
@@ -7,22 +7,22 @@ export default [
         },
     },
     {
-        description: 'Adding O to words ending with T',
-        find: /t$/gi,
-        replace: function (match) {
-            return match + 'o'
+        description: 'Replace J with I',
+        find: /j/gi,
+        replace: function () {
+            return 'y'
         },
     },
     {
         description: 'VI to BI when word starts with VI',
-        find: /vi/gi,
+        find: /^vi/gi,
         replace: function () {
             return 'bi'
         },
     },
     {
         description: 'Add U between to vowels',
-        find: /(b|d|f|g|h|j|k|l|m|p|q|r|s|v|x|z){2}/gi,
+        find: /(b|d|f|g|h|j|k|l|m|p|q|r|s|t|v|x|z){2}/gi,
         replace: function (match) {
             return match[0] + 'u' + match[1]
         },
@@ -40,5 +40,12 @@ export default [
         replace: function (match) {
             return match + 'u'
         }
+    },
+    {
+        description: 'Adding O to words ending with T',
+        find: /t$/gi,
+        replace: function (match) {
+            return match + 'o'
+        },
     },
 ]
