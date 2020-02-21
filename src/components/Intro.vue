9f776de97a7ffb72ffde8bb73e9cc3b9ed0c4282
Vincent Lejtzén <vincent@lejtzendesign.se>
2020-02-21 17:05:27 +0100

    Fixes

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,18 +1,33 @@
 <template>
-    <div class="intro__symbol">
+    <div class="intro__symbol" :style="styles">
         {{ symbol }}
     </div>
 </template>
 
 <script>
     export default {
-        data: function () {
+        data() {
             return {
                 symbol: '日',
+                count: 0,
             }
         },
 
-        mounted: function () {
+        computed: {
+            styles() {
+                let positive = this.count
+                let negative = (this.count - (this.count * 2))
+
+                let up = negative + 'rem 0 0 #2be2e2'
+                let down = positive + 'rem 0 0 #D41D1D'
+
+                return {
+                    'text-shadow': up + ', ' + down,
+                }
+            },
+        },
+
+        mounted() {
             let timing = 100
             let symbols = [
                 '本',
@@ -36,6 +51,7 @@
             symbols.forEach((item, index) => {
                 setTimeout(() => {
                     this.symbol = item
+                    this.count++
                 }, index * timing)
             })
 
