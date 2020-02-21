15fea9898284429c89a624581479a1df68316870
Vincent Lejtzén <vincent@lejtzendesign.se>
2020-02-21 10:18:15 +0100

    Basic layout.

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,6 +1,46 @@
 <template>
-    <div class="layout">
-        Layout
+    <div class="app">
+        <transition>
+            <div class="intro" v-if="isLoading">
+                {{ symbol }}
+            </div>
+
+            <div class="layout" v-else>
+                <h1 class="logo">
+                    <a href="/" title="Japanizer">
+                        <span class="logo__symbol">翻</span>
+                        <span class="logo__wordmark">Japanizer</span>
+                    </a>
+                </h1>
+
+                <button class="toggle-console" :class="{ 'toggle-console--active': showConsole }" type="button" name="openConsole" @click="showConsole = !showConsole">
+                    []
+                </button>
+
+                <div class="panels">
+                    <div class="character">
+                        CHAR :)
+                    </div>
+                    <div class="panel panel--input">
+                        <div class="content">
+                            Input
+                        </div>
+                    </div>
+                    <div class="panel panel--output">
+                        <div class="content">
+                            Output
+
+                            <button type="button" name="shareOutput">[] Share</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="console" :class="{ 'console--active': showConsole }">
+                    <div class="console__inner">
+                        Console
+                    </div>
+                </div>
+            </div>
+        </transition>
     </div>
 </template>
 
@@ -8,6 +48,9 @@
     export default {
         data: function () {
             return {
+                isLoading: true,
+                symbol: '翻',
+                showConsole: false,
             }
         },
 
@@ -18,6 +61,7 @@
         },
 
         mounted: function () {
+            this.isLoading = false
         },
 
         watch: {
