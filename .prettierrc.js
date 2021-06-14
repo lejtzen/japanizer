d35fe9a789c70437c15518e2ce77b406f300511b
Vincent Lejtzén <vincent@lejtzendesign.se>
2021-06-14 22:33:38 +0200

    Add Prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    tabWidth: 4,
+    singleQuote: true,
+    semi: false,
+    trailingComma: 'all',
+    overrides: [
+        {
+            files: ['*.json'],
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
+}
