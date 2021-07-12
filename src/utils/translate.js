cfefe86ae90eba7addcc2cbe4d29b1260454b130
Vincent Lejtzén <vincent@lejtzendesign.se>
2021-07-12 12:30:24 +0200

    Keep track of casing

--- src/utils/translate.js
+++ src/utils/translate.js
@@ -1,16 +1,28 @@
 import rules from '@/utils/rules.js'
 
+const isUpperCase = (input) => {
+    return input === input.toUpperCase()
+}
+
+const capitalize = (input) => {
+    return input.charAt(0).toUpperCase() + input.slice(1)
+}
+
 export default function (text) {
     let separator = ' '
     let words = text.split(separator)
 
     return words
-        .map((word, index) => {
-            let original = word
-
+        .map((word) => {
             rules.forEach((rule) => {
                 word = word.replace(rule.find, (match) => {
-                    return rule.replace(match)
+                    const result = rule.replace(match)
+
+                    if (isUpperCase(match[0])) {
+                        return capitalize(result)
+                    }
+
+                    return result
                 })
             })
 
